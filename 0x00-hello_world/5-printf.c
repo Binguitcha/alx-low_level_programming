4b0626faad9672266aa64d638203944ddfcd87dd
Binguitcha <lemailbinguoshadownet@gmail.com>
2022-09-08 18:30:06 -0700

    tsk 4_6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - beguinning
+ *
+ * Return: Always 0 (Success)
+ */
+int main(avoid)
+{
+	print("with proper grammar; but the outcome is piece of art,\n");
+	return (0);
+}
